fdf5dd5cc0c4d6c58d8a2ce97ebcd16f8a2775b0
Dell Du <18588220928@163.com>
2018-12-05 15:33:19 +0800

    Update.

---
+++ python/sqlite3_demo.py
@@ -0,0 +1,20 @@
+import sqlite3  
+  
+db = sqlite3.connect('test.db')  
+cur = db.cursor()  
+  
+cur.execute("CREATE TABLE if not exists t (b BLOB);")  
+  
+with open('0.bin', 'rb') as f:  
+    for i in range(10000):
+    	cur.execute("insert into t values(?)", (sqlite3.Binary(f.read()), ))  
+    db.commit()  
+  
+cur.execute('select b from t limit 1')  
+b = cur.fetchone()[0]  
+  
+with open('00.bin', 'wb') as f:  
+    f.write(b)  
+  
+db.close()
+
