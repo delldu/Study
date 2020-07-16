dc2b97fb2f9b1d7b318076914d31af2879e35758
Dell Du <18588220928@163.com>
2020-07-17 01:27:34 +0800

    Update.

--- web/mask/mask.js
+++ web/mask/mask.js
@@ -2,15 +2,31 @@ function ChoseFile(buttonName) {
 	document.getElementById(buttonName).click();
 }
 
+function ReadImageData(img, fileobj) {
+	// img --  image element
+	img.onload = function() {}
+	var reader = new FileReader();
+	reader.onload = function(evt) {
+		img.src = evt.target.result;
+	}
+	reader.readAsDataURL(fileobj);
+}
+
+
 function PreviewImage(file, canvasname) {
 	if (file.files && file.files[0]) {
 		var img = document.getElementById(canvasname);
-		img.onload = function() {}
-		var reader = new FileReader();
-		reader.onload = function(evt) {
-			img.src = evt.target.result;
+		ReadImageData(img, file.files[0]);
+
+		if (canvasname == "source_canvas") {
+			var blend_fg = document.getElementById("blend_fg_canvas");
+			ReadImageData(blend_fg, file.files[0]);
+			// blend_fg.src = img.src;
 		}
-		reader.readAsDataURL(file.files[0]);
+
+		if (canvasname == "mask_canvas") {
+			var blend_bg = document.getElementById("blend_bg_canvas");
+			blend_bg.style.backgroundImage = "url('" + file.files[0].name + "')";
+		}
 	}
 }
-
