932d53bfd07e2fbc1747f60e7751d7704791e320
delldu <18588220928@163.com>
2021-11-14 20:19:55 +0800

    Update

--- python/blender_addon/ai_video/video.py
+++ python/blender_addon/ai_video/video.py
@@ -22,6 +22,9 @@ from app import VideoCommand
 import subprocess as sp
 import numpy as np
 
+
+# Good reference
+# https://github.com/Zulko/moviepy.git
 class VideoReader(object):
     """Use ffmpeg process video"""
 
@@ -30,7 +33,7 @@ class VideoReader(object):
         self.n_frames = 0
         self.height = 256
         self.width = 256
-        self.fps = 24
+        self.fps = 24.000
         self.probe()
 
     def probe(self):
@@ -64,16 +67,38 @@ class VideoReader(object):
             if g:
                 self.n_frames = int(g[0])
 
-    def forward(self, callback=None):
+    def forward(self, start_frame=0, stop_frame=-1, callback=None):
+        # [start_frame, stop_frame)
+
         if self.n_frames < 1:
             return 0
+
+        if start_frame < 0:
+            start_frame = 0
+
+        if stop_frame < 0:
+            stop_frame = self.n_frames
+        stop_frame = min(stop_frame, self.n_frames)
+
+        if start_frame >= stop_frame:
+            return 0
+
         if callback is None:
             callback = self.print_frame
+
+        if start_frame > 0:
+            start_time = int(start_frame/self.n_frames)
+        else:
+            start_time = 0
+        start_pos = int(self.fps * start_time + 0.0001)
 
         # rgba format
         buffer_size = self.height * self.width * 4
         cmd = [
             "ffmpeg",
+            # start time
+            "-ss",
+            "%.06f" % start_time,
             # input
             "-i",
             self.filename,
@@ -92,24 +117,38 @@ class VideoReader(object):
         except:
             print(f"read {self.filename} frame error.")
             return 0
-        for frame_no in range(self.n_frames):
+
+        frame_count = 0
+        while start_pos < stop_frame:
             buffer = proc.stdout.read(buffer_size)
+            start_pos = start_pos + 1 # ==> current frame no is start_pos -1
+
             if len(buffer) != buffer_size:
-                print(f"frame buffer != buffer_size ?")
+                print(f"frame read error: data length != buffer_size")
                 continue
+
+            # skip frames ?
+            if (start_pos - 1) < start_frame:
+                continue
+
             frame_data = np.frombuffer(buffer, np.uint8).reshape(
                 [self.height, self.width, 4]
             )
-            callback(frame_no, frame_data)
-        proc.terminate()
+            callback(start_pos - 1, frame_data)
+            frame_count = frame_count + 1
 
-        return frame_no + 1
+        proc.terminate()
+        proc.stdout.close()
+        proc.stderr.close()
+        proc.wait()
+
+        return frame_count
 
     def print_frame(self, no, data):
         print(f"frame: {no} -- {data.shape}")
 
-    def __call__(self, callback=None):
-        return self.forward(callback)
+    def __call__(self, start_frame=0, stop_frame=-1, callback=None):
+        return self.forward(start_frame, stop_frame, callback)
 
     def __repr__(self):
         return f"{self.filename}: n_frames={self.n_frames}, wxh={self.width}x{self.height}, fps={self.fps:.3f}"
@@ -120,6 +159,7 @@ def test_video_reader():
     print(video)
     video.forward()
     # video()
+    pdb.set_trace()
 
 
 class VideoClient(NCClient):
